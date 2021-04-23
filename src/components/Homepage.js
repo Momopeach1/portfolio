854612e747b57f62a0f20b27c2aff81c4fef54d3
Mohammed Elsayed <moerty43@gmail.com>
2021-04-23 15:48:08 -0400

    button styling
    fixed button styling

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
 import '../styles/Homepage.css';
 
+//button styles
+const useStyles = makeStyles({
+    root: {
+        background: 'transparent',
+        borderRadius: 4,
+        border: '1px solid',
+        borderColor: '#64ffda',
+        height: 'fit-content',
+        width: 'fit-content',
+        display: 'block',
+        padding: '0.75rem 1rem',
+        marginTop: '50px',
+        '&:hover': {
+            backgroundColor: '#64ffda24',
+        },
+    },
+    label: {
+        color:'#64ffda',
+        textTransform: 'none',
+        fontSize: '16px',
+        fontFamily: 'whitney-book',
+    },
+});
+
 const Homepage = () => {
+
+    const classes = useStyles();
+
     return (
         <div className='main-content'>
             <div className='img-container'>
@@ -21,6 +51,14 @@ const Homepage = () => {
                     a website dedicated to sharing art between 
                     Athens, Greece and Bronx, New York.
                 </p>
+                <Button
+                    component={Link} to={'/#'}
+                    classes={{
+                        root: classes.root,
+                        label: classes.label,
+                    }}
+                    >
+                    Resume</Button>
             </div> 
         </div>
     )
