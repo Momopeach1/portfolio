6f108588db5308a9f51b8ddfd40c8f755e94a581
Mohammed Elsayed <moerty43@gmail.com>
2021-04-28 13:57:53 -0400

    push for laptop
    unfinished code to work on from a different machine

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import '../styles/Project.css'
+
 const Projects = () => {
     return (
         <div className='main-content'>
-            <h1>
-                Some Things I've Built
-            </h1>
+            <div className='text-container-project'>
+                <div className='text-heading-project'>
+                    <h1 className='h1-about-flare'>
+                        01. 
+                    </h1>
+                    <h1 className='h1-about'>
+                        Some Things I've Built
+                    </h1>
+                    <div className='vline'></div>
+                </div>
+            </div>
+            <img className='project-img' alt='borg home' src='/borg_home.png'></img>
         </div>
     )
 }
