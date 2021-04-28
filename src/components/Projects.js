8202c452c83c9e22fb85fe6cdde69acc87087b62
Mohammed Elsayed <moerty43@gmail.com>
2021-04-28 17:08:47 -0400

    Projects Page Update
    Added a card and styling to projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { MenuItem } from "react-pro-sidebar";
+import { FiGithub } from "react-icons/fi";
 import '../styles/Project.css'
 
 const Projects = () => {
@@ -19,10 +20,21 @@ const Projects = () => {
             <div className='project-main-container'>
                 <div className='project-card'>
                     <img className='project-img' alt='borg home' src='/borg_home.png'></img>
-                    <div className='project-card-text'>
+                    <div className='project-card-text-container'>
+                        <h2 className='project-title-flare'>Project being improved</h2>
+                        <h1 className='project-title'>Walkie Talkie</h1>
                         <p className='project-card-desc'>
-                            project
+                            An instant messaging/VoIP platform that allows users to connect over the internet.
+                            It has a built in collaborative whiteboard that allows users to draw together.
                         </p>
+                        <ul className='project-tech-list'>
+                            <li>React</li>
+                            <li>Node.js</li>
+                            <li>Express.js</li>
+                            <li>WebRTC</li>
+                            <li>Socket.io</li>
+                            <li>MongoDB</li>
+                        </ul>
                     </div>
                 </div>
             </div>
